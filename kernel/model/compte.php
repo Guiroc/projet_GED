498aa7b323d24171b3d72977f683006ce0c4dbfd
BONNEAU Guillaume <gbonneau@bts.sio>
2016-09-27 11:06:45 +0200

    0.0.4

--- kernel/model/compte.php
+++ kernel/model/compte.php
@@ -24,33 +24,33 @@
 			unset($mdp_utilisateur);
 		}
 		//set
-		public set_id_utilisateur($id_utilisateur){
+		public function set_id_utilisateur($id_utilisateur){
 			
 			$this->id_utilisateur = $id_utilisateur;
 		}
 		
-		public set_pseudo_utilisateur($pseudo_utilisateur){
+		public function set_pseudo_utilisateur($pseudo_utilisateur){
 			
 			$this->pseudo_utilisateur = $pseudo_utilisateur;
 		}
 		
-		public set_mdp_utilisateur($mdp_utilisateur){
+		public function set_mdp_utilisateur($mdp_utilisateur){
 			
 			$this->mdp_utilisateur = $mdp_utilisateur;
 		}
 		
 		//get
-		public get_id_utilisateur(){
+		public function get_id_utilisateur(){
 			
 			return ($this->id_utilisateur);
 		}
 		
-		public get_pseudo_utilisateur(){
+		public function get_pseudo_utilisateur(){
 			
 			return ($this->pseudo_utilisateur);
 		}
 		
-		public get_mdp_utilisateur(){
+		public function get_mdp_utilisateur(){
 			
 			return ($this->mdp_utilisateur);
 		}
